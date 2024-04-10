a8a4717e95066d05a3f29b9a7d73204d0318ae89
Keiran <kjensen4@unca.edu>
2024-04-10 11:17:35 -0400

    Homework 5

---
+++ homework/homework5/carousel.js
@@ -0,0 +1,50 @@
+const photos = [
+    "images/img1-600x400.jpg",
+    "images/img2-600x400.jpg",
+    "images/img3-600x400.jpg",
+    "images/img4-600x400.jpg",
+    "images/img5-600x400.jpg",
+    "images/img6-600x400.jpg",
+    "images/img7-600x400.jpg",
+    "images/img8-600x400.jpg",
+    "images/img9-600x400.jpg",
+    "images/img10-600x400.jpg"
+];
+let idx = 0;
+
+
+/* This function should:
+    (1) display the new image inside of the .current-photo container, and 
+    (2) update the caption inside of the .caption paragraph
+*/
+function showImage() {
+    let currentphoto = document.querySelector('.current-photo>img');
+    let caption = document.querySelector('.caption');
+
+    currentphoto.src = photos[idx];
+    caption.innerHTML = `Image ${idx+1} of ${photos.length}`;
+}
+
+
+/* This function should set the idx variable 
+   to one greater than the current value of idx, 
+   and then invoke the showImage() function.
+   If the idx gets to one less than the length of 
+   the array, set idx to 0.
+*/
+function forward() {
+    idx = (idx + 1) % photos.length     // Increment with wrap
+    showImage();
+}
+
+
+/* This function should set the idx variable 
+   to one less than the current value of idx, 
+   and then invoke the showImage() function.
+   If the idx gets to the beginning, set idx to
+   one less than the length of the array.
+*/
+function back() {
+    idx = (idx + photos.length - 1) % photos.length     // Decrement with wrap
+    showImage();
+}
